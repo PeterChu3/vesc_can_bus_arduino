70312c84e9e82e8e9d59e50fd3f5535a747a96b7
Peter <chuy1483@gmail.com>
2023-01-10 02:03:41 -0500

    Compiles and marks as message recieved for mesg 6

--- examples/vesc_can_bus/vesc_can_bus.ino
+++ examples/vesc_can_bus/vesc_can_bus.ino
@@ -1,11 +1,10 @@
 #include <mcp_can.h>
-#include <vesc_can_bus.h>
+#include <vesc_can_bus_arduino.h>
 
-CAN can;             // get torque sensor data, throttle for now
+CAN can; // get torque sensor data, throttle for now
 
 bool print_realtime_data;
 long last_print_data;
-
 
 void setup_()
 {
@@ -13,20 +12,19 @@ void setup_()
     Serial.begin(115200); // Wired comms from USB port
 
     can.initialize();
-
 }
 
 void loop()
 {
     can.spin();
 
-    //can.vesc_set_current(2); //2 amps of current
+    // can.vesc_set_current(2); //2 amps of current
 
     if (print_realtime_data == true)
     {
         if (millis() - last_print_data > 200)
         {
-            Serial.print(can.erpm); 
+            Serial.print(can.erpm);
             Serial.print(can.inpVoltage);
             Serial.print(can.dutyCycleNow);
             Serial.print(can.avgInputCurrent);
--- vesc_can_bus_arduino.h
+++ vesc_can_bus_arduino.h
@@ -4,30 +4,25 @@ class CAN
 {
 
 public:
+#define CAN0_INT 2 // Set INT to pin 2
 
-#define CAN0_INT 21                              // Set INT to pin 21
+    long unsigned int rxId;
+    unsigned char len = 0;
+    unsigned char rxBuf[8];
+    char msgString[128]; // Array to store serial string
 
-long unsigned int rxId;
-unsigned char len = 0;
-unsigned char rxBuf[8];
-char msgString[128];// Array to store serial string
+    float inpVoltage, dutyCycleNow, avgInputCurrent, avgMotorCurrent, tempFET, tempMotor;
+    int32_t erpm;
 
+    void initialize();
+    void spin();
+    void get_frame();            // populates rxId and rxBuf with latest can frame
+    void can_send(byte data[8]); // transmits the send commands to the sensor
+    void print_raw_can_data();   // output raw can data to terminal (debug)
 
-float inpVoltage, dutyCycleNow, avgInputCurrent, avgMotorCurrent, tempFET, tempMotor;
-long erpm;
-
-
-void initialize();
-void spin();
-void get_frame(); // populates rxId and rxBuf with latest can frame 
-void can_send(byte data[8]); //transmits the send commands to the sensor
-void print_raw_can_data(); //output raw can data to terminal (debug)
-
-
-void vesc_set_duty(float duty);
-void vesc_set_current(float current);
-void vesc_set_erpm(float erpm);
-float process_data_frame_vesc(char datatype, unsigned char byte1, unsigned char byte2);
-int hex2int(char buf[]);
-
+    void vesc_set_duty(float duty);
+    void vesc_set_current(float current);
+    void vesc_set_erpm(float erpm);
+    float process_data_frame_vesc(char datatype, unsigned char byte1, unsigned char byte2);
+    int hex2int(char buf[]);
 };
